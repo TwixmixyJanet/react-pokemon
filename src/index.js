5951c2b1b55cc114e10d9d5ab80058a77f6611cb
Janet Webster <twixmixy@gmail.com>
2024-02-21 14:07:33 -0500

    Displaying base pokemon on page. Fixed service worker / manifest issue.

--- src/index.js
+++ src/index.js
@@ -1,5 +1,7 @@
 import React from "react";
-import ReactDOM from "react-dom/client";
+import ReactDOM from "react-dom";
 import App from "./App";
 
-ReactDOM.createRoot(document.getElementById("root"));
+ReactDOM.render(<App />, document.getElementById("root"));
+
+// index.js:5 Warning: ReactDOM.render is no longer supported in React 18. Use createRoot instead. Until you switch to the new API, your app will behave as if it's running React 17. Learn more: https://reactjs.org/link/switch-to-createroot
